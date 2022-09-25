af2cd05f5af2c67d6f8d22475c90946e732c1a1b
gabrielsilvagaldino <gabrielgaldino3004@gmail.com>
2022-09-25 10:10:17 -0300

    clicker feito com sucesso, agora falta fazer os upgrades e fazer com que o valor do clique mude de acordo com os upgrades

--- clicker/src/components/Clicker.js
+++ clicker/src/components/Clicker.js
@@ -1,13 +1,32 @@
 import React from "react";
+import store from "../redux";
+import { connect } from "react-redux";
 
 class Clicker extends React.Component {
+
+  handleOnClick = () => {
+    const info = store.getState()
+    const action = { type: 'CLICK_BUTTON', payload: info.clickReducer.clickValue }
+    console.log(action);
+    this.props.dispatch(action)
+  }
   render() {
     return(
       <div>
-        Clicker
+        <p>{ store.getState().clickReducer.totalValue } cortes</p>
+        <button
+        type="button"
+        onClick={this.handleOnClick}
+        >
+        BOTAO
+        </button>
       </div>
     );
   }
 }
 
-export default Clicker;
+const mapStateToProps = (state) => {
+  return state;
+}
+
+export default connect(mapStateToProps)(Clicker);
